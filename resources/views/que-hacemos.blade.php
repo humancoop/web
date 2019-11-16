a627e364720e999307b313094195a9f43567074d
José Ignacio Amelivia <ohcan2@gmail.com>
2019-11-16 14:00:43 +0100

    Deleting Otros

--- resources/views/que-hacemos.blade.php
+++ resources/views/que-hacemos.blade.php
@@ -15,15 +15,5 @@
 			</div>
 			</a>
 		</div>
-		<div class="w-1/2 p-4">
-			<a href="">
-			<div class="max-w-sm rounded overflow-hidden shadow-lg">
-				<img class="w-full" src="{{ asset('assets/images/manos.jpg') }}"/>
-				<div class="px-6 py-4">
-					<div class="font-bold text-xl mb-2">Otros</div>
-				</div>
-			</div>
-			</a>
-		</div>
 	</div>
 @stop
