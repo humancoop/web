3d1b619207c2c0d6befd2f745ccd563fd9a5cf2f
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-03-14 14:28:25 +0100

    Update more social media links

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,13 +18,13 @@
 							</div>
 							<div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
 								<div class="text-sm lg:flex-grow">
-									<a href="https://facebook.com/humancoop" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+									<a href="https://facebook.com/humancoop.ong.9" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
 										<i class="fab fa-facebook-square"></i>
 									</a>
-									<a href="https://instagram.com/humancoop" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+									<a href="https://instagram.com/ong.humancoop" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
 										<i class="fab fa-instagram"></i>
 									</a>
-									<a href="https://twitter.com/humancoop" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+									<a href="https://twitter.com/human_coop" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
 										<i class="fab fa-twitter-square"></i>
 									</a>
 									<a href="email" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
