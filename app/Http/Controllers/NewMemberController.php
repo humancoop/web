dc935f8a7aff4ce819b8b42ce66fa39520f05429
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-05-03 20:46:31 +0200

    Validate new member form

--- app/Http/Controllers/NewMemberController.php
+++ app/Http/Controllers/NewMemberController.php
@@ -3,12 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\NewMemberInfo;
 
 class NewMemberController extends Controller
 {
     public function newMemberFormPost(Request $request) {
+
+         $validator = Validator::make($request->all(), [
+            'full_name' => 'required',
+            'birthplace' => 'required',
+            'birthdate' => 'required',
+            'province' => 'required',
+            'postal_code' => 'required',
+            'address' => 'required',
+            'email' => 'required',
+            'city' => 'required',
+            'phone_number' => 'required',
+            'secondary_phone_number' => 'required',
+            'nif' => 'required',
+            'passport_number' => 'required',
+            'father_name' => 'required',
+            'mother_name' => 'required',
+            'issue_date' => 'required',
+            'issue_location' => 'required',
+            'validity_date' => 'required',
+            'account_number' => 'required',
+            'training' => 'required',
+            'professional_experience' => 'required',
+            'years_of_experience' => 'required',
+            'speciality' => 'required',
+            'motivations' => 'required',
+            'volunteering_experience_info' => 'required',
+            'where_did_you_know' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('socio')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
         NewMemberInfo::create($request->input());
+
         return view('socio');
     }
 
--- resources/views/socio.blade.php
+++ resources/views/socio.blade.php
@@ -12,7 +12,9 @@
                     Nombre y apellidos
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="full_name" name="full_name" type="text" placeholder="Nombre y apellidos">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('full_name')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -21,14 +23,18 @@
                     Lugar de nacimiento
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="birthplace" name="birthplace" type="text" placeholder="Lugar de nacimiento">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('birthdate')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Fecha de nacimiento
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="birthdate" name="birthdate" type="date" placeholder="Fecha de nacimiento">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('birthplace')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-12">
@@ -37,14 +43,18 @@
                     Provincia
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="province" name="province" type="select" placeholder="Provincia">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('province')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Código postal
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="postal_code" name="postal_code" type="text" placeholder="Código postal">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('postal_code')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -53,7 +63,9 @@
                     Dirección
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="address" name="address" type="text" placeholder="Domicilio">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('address')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -62,14 +74,18 @@
                     Localidad
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="city" name="city" type="text" placeholder="Localidad">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('city')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Provincia
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="province" name="province" type="select" placeholder="Provincia">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('province')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-12">
@@ -78,21 +94,27 @@
                     Email
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="email" name="email" type="email" placeholder="E-mail">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('email')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Teléfono móvil
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="phone_number" name="phone_number" type="tel" placeholder="Teléfono móvil">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('phone_number')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Otro número de contacto
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="secondary_phone_number" name="secondary_phone_number" type="tel" placeholder="Otro móvil de contacto">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('secondary_phone_number')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -101,14 +123,18 @@
                     Nif
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="nif" name="nif" type="text" placeholder="Nif">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('nif')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Número de pasaporte
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="passport_number" name="passport_number" type="text" placeholder="Número de pasaporte">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('passport_number')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -117,14 +143,18 @@
                     Nombre del padre
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="father_name" name="father_name" type="text" placeholder="Nombre del padre">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('father_name')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Nombre de la madre
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="mother_name" name="mother_name" type="text" placeholder="Nombre de la madre">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('mother_name')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-12">
@@ -133,21 +163,27 @@
                     Fecha de expedición
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="issue_date" name="issue_date" type="date" placeholder="Fecha de expedición">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('issue_date')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Lugar de expedición
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="issue_location" name="issue_location" type="text" placeholder="Lugar de expedición">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('issue_location')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Fecha de validez
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="validity_date" name="validity_date" type="date" placeholder="Fecha de validez">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('issue_date')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-12">
@@ -162,7 +198,9 @@
                     Número de cuenta voluntario
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="account_number" name="account_number" type="text" placeholder="Número de cuenta voluntario">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('account_number')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-12">
@@ -179,14 +217,18 @@
                     Formación
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="training" name="training" type="text" placeholder="Formación">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('training')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Especialidad
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="speciality" name="speciality" type="text" placeholder="Especialidad">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('speciality')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -195,14 +237,18 @@
                     Experiencia profesional
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="professional_experience" name="professional_experience" type="text" placeholder="Experiencia profesional">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('professional_experience')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Años de experiencia
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="years_of_experience" name="years_of_experience" min="0" type="number" placeholder="Años de experiencia">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('years_of_experience')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -211,7 +257,9 @@
                     Motivaciones
                 </label>
                 <textarea class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="motivations" name="motivations" type="text" placeholder="Motivaciones"></textarea>
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('motivations')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-12">
@@ -226,7 +274,9 @@
                     Actividad desarrollada
                 </label>
                 <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="volunteering_experience_info" name="volunteering_experience_info" type="text" placeholder="Actividad desarrollada">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @error('volunteering_experience_info')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-12">
@@ -242,8 +292,10 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     ¿Dónde nos conociste?
                 </label>
-                <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="professional_experience" name="professional_experience" type="text" placeholder="Experiencia profesional">
-                <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                <input class="appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="where_did_you_know" name="where_did_you_know" type="text" placeholder="Experiencia profesional">
+                @error('where_did_you_know')
+                    <p class="text-red-500 text-xs italic">Por favor, rellena este campo.</p>
+                @enderror
             </div>
         </div>
         <div class="md:flex md:items-center">
