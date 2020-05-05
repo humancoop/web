c9d2ef9e4dbd7a2f8a97eb3221f7fcdd082847c7
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-05-05 18:59:18 +0200

    Add links for the organization spotify playlist

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,6 +27,9 @@
 									<a href="https://twitter.com/human_coop" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
 										<i class="fab fa-twitter-square"></i>
 									</a>
+									<a href="https://open.spotify.com/user/31umi7ntu6zmm5nhl3u5etfhjnqy?si=EyPFgzvyQpmBsdxHnGbzXQ" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+										<i class="fab fa-spotify"></i>
+									</a>
 									<a href="email" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
 										<i class="fas fa-envelope-square"></i>
 									</a>
