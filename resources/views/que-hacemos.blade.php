8028a36993324934e36d2212f5e2e2008a468098
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-05-19 16:33:21 +0200

    Small style fix

--- resources/views/que-hacemos.blade.php
+++ resources/views/que-hacemos.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   <h1 class="text-6xl text-center">¿Qué hacemos?</h1>
 	<div class="flex mb-4">
-		<div class="w-1/2 p-4">
+		<div class="p-4">
 			<a href="/territorios-liberados-saharauis">
 			<div class="max-w-sm rounded shadow-lg p-4 bg-white">
                 <thumbor
